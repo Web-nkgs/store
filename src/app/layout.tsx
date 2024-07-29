b459f3fde7314043bd6a653ca70ceca1ffee0831
neddyKG <gonzalezneddy@gmail.com>
2024-07-29 11:36:26 -0400

    Added link to navigate from the nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,8 +28,20 @@ export default function RootLayout({
         <header>
           <nav>
             <ul>
-              <li>Home</li>
-              <li>Store</li>
+              {/* Using Link allows us to reduce bandwidth cause it caches the files.
+              Since Next.js renders on the server side we do less petitions 
+              avoiding the reload (done with anchor <a>).
+              And navigation is waymore fluid, since it doesn't reload.  
+              Use link = internal app urls.
+              Use anchor = for external url links ex.- google.com.
+              */}
+              <Link href={"/"}>
+                <li>Home</li>
+              </Link>
+
+              <Link href={"/store"}>
+                <li>Store</li>
+              </Link>
             </ul>
           </nav>
         </header>

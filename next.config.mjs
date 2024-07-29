94212d3ac29568aa8dc66204082630716c1fea91
neddyKG <gonzalezneddy@gmail.com>
2024-07-29 11:07:36 -0400

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+/* This configuration file is very important, here we'll set 
+SASS compilers, dynamic imports, webpack configs.
+*/
 const nextConfig = {};
 
 export default nextConfig;

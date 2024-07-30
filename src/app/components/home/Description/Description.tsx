1b9e71c60935debf643c858ab027527b8da397a1
neddyKG <gonzalezneddy@gmail.com>
2024-07-30 08:06:53 -0400

    Added styles and corresponding text to Description comp.

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -1,11 +1,15 @@
+import styles from './Description.module.sass'
+
 export const Description = () => {
-    return <section>
+    return <section className={styles.Description}>
         {/* we could access directly the directories in public (images, videos, files, etc.) anything static. 
         TIP: Only add super necessary static elements to you app. If you have too much images/videos, a better
         solution is a cloud bucket like S3.
         */}
         <img src="/images/description.jpeg" alt="product marketplace" />
-        <h2>Description</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga assumenda, nulla quibusdam vitae quis ipsam aperiam ex eos iure ut? Harum iusto sed nam autem praesentium quas consequatur? Quibusdam.</p>
+        <div className={styles.Description__text}>
+            <h2>Bring the future today</h2>
+            <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+        </div>
     </section>
 }

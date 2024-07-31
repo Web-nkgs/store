9a55246b3ec7f702d8637419b16caf3e9161c530
neddyKG <gonzalezneddy@gmail.com>
2024-07-31 09:20:04 -0400

    Added as an example an exposed environment variable with NEXT_PUBLIC prefix and a template of env file

--- src/app/components/home/MainProducts/MainProducts.tsx
+++ src/app/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,7 @@
+"use client"
 export const MainProducts = () => {
+    console.log("env var: ", process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+    
     return <section>
         <h1>MainProducts!</h1>
     </section>

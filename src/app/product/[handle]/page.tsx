a4bd31f3b6f7a951e91054a4c99070967935a9e0
neddyKG <gonzalezneddy@gmail.com>
2024-08-05 17:04:13 -0400

    Added product view

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -3,42 +3,47 @@ Params are useful when we know the resource we want to obtain, ex. a product whe
 SearchParams are useful for adding page config, search token, even analytics and AB testing.
 
  */
+
+import { ProductView } from "app/components/product/ProductView";
+import { getProducts } from "app/services/shopify/products";
+
 // Server component
-// interface ProductPageProps {
-//     params: {
-//         handle: string
-//     },
-//     searchParams: {
-//         id: string
-//     }
-// }
-// export default async function ProductPage(props: ProductPageProps) {
-//     const { handle }  = props.params
-//     console.log("server component params:", handle);
-//     console.log("server component searchParams: ", props.searchParams.id);
-    
-//     return <h1>Product page</h1>
-// }
+
+interface ProductPageProps {
+  searchParams: {
+    id: string
+  }
+}
+
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+
+  return <>
+ {products && products[0] ? <ProductView product={products[0]} /> : 
+ <h1>No existe el producto</h1>} 
+  </>
+}
 
 // ---------------------------------------------------
 
 // Client component
-"use client"
+// "use client"
 
-import { useParams, useSearchParams } from "next/navigation"
+// import { useParams, useSearchParams } from "next/navigation"
 
-interface ProductPageProps {
-    searchParams: {
-        id: string
-    }
-}
+// interface ProductPageProps {
+//     searchParams: {
+//         id: string
+//     }
+// }
 
-export default function ProductPage(props: ProductPageProps) {
-    const params = useParams()
-    console.log("params: ", params);
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
-    console.log("searchParams id: ", id);
+// export default function ProductPage(props: ProductPageProps) {
+//     const params = useParams()
+//     console.log("params: ", params);
+//     const searchParams = useSearchParams()
+//     const id = searchParams.get("id")
+//     console.log("searchParams id: ", id);
     
-    return <h1>Product page</h1>
-}
+//     return <h1>Product page</h1>
+// }

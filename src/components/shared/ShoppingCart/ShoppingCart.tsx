df73e4d3a8a2bb435fca3950192f9f046bd65938
neddyKG <gonzalezneddy@gmail.com>
2024-08-10 10:19:18 -0400

    Added cart detail on cart click

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,16 +1,40 @@
 "use client"
 import { useShoppingCart } from "app/hooks/useShoppingCart"
+import { useState } from "react"
 import { FaShoppingCart } from "react-icons/fa"
 import styles from "./ShoppingCart.module.sass"
 
 export const ShoppingCart = () => {
     const { cart } = useShoppingCart()
-    console.log("cart: ", cart);
-    
-    return <button className={styles.ShoppingCart}>
+    const [isCartItemsOpen, setIsCartItemsOpen] = useState(false)
+
+    const handleIsCartItemsOpen = () => {
+        setIsCartItemsOpen(!isCartItemsOpen)
+    }
+
+    return <div  className={styles.ShoppingCart}>
+        <button
+        onClick={handleIsCartItemsOpen}
+        className={styles.ShoppingCart__cartButton}>
         <span className={styles.ShoppingCart__counter}>
             {cart.length}
         </span>
         <FaShoppingCart />
     </button>
+    {
+            isCartItemsOpen && cart.length > 0 &&
+            <div className={styles.ShoppingCart__items}>
+                {
+                    cart.map((item) => (
+                        <div key={item.id}>
+                            <p>{item.title}</p>
+                            <p>Cantidad: {item.quantity}</p>
+                            <hr />
+                        </div>
+                    ))
+                }
+                <button className={styles.ShoppingCart__buyButton}>Buy</button>
+            </div>
+        }
+    </div>
 }

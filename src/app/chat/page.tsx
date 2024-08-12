d35bd288f24cb5e87bae157e04c207d1a7095b69
neddyKG <gonzalezneddy@gmail.com>
2024-08-12 16:50:42 -0400

    Added agent to recommend products (google AI needs to be updated, view the TO-DO comment)

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,13 @@
 import Chat from "app/components/chat/chat";
+import { getProducts } from "app/services/shopify/products";
+import { createAgent } from "app/utils/googleAi/createAgent";
 
 
-export default function ChatPage() {
- return <Chat/>
+export default async function ChatPage() {
+    const products = await getProducts()
+    const productTitles = products.map((product) => product.title)
+    const flatProductTitles = productTitles.join('/n')
+    const agent = createAgent(flatProductTitles)
+
+    return <Chat agent={agent}/>
 }
--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -2,25 +2,38 @@
 
 import { useChat } from 'ai/react';
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+// TO-DO: posible solution for product recommendations: https://sdk.vercel.ai/providers/ai-sdk-providers/google-generative-ai#google-generative-ai-provider.
+// This is not complete, since the model can hallucinate features of the products, so we have to improve how we manage the AI with different tools (ex. langChain)
+export default function Chat(props: { agent: string }) {
 
-  return (
-    <div>
-      {messages.map(m => (
-        <div key={m.id}>
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        initialMessages: [
+            {
+                id: '1',
+                role: 'system',
+                content: props.agent
+            }
+        ]
+    });
+
+    return (
+        <div>
+            {messages
+                .filter((m) => m.role !== "system")
+                .map(m => (
+                    <div key={m.id}>
+                        {m.role === 'user' ? 'User: ' : 'AI: '}
+                        {m.content}
+                    </div>
+                ))}
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={input}
+                    placeholder="Say something..."
+                    onChange={handleInputChange}
+                />
+            </form>
         </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
-  );
+    );
 }
